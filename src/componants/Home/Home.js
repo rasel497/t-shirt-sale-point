49f5235057488bb67a89d99233f8ceaada1399e2
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-09 00:13:53 +0600

    add wrap button in arrow func get product

--- src/componants/Home/Home.js
+++ src/componants/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Home.css'
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 import Tshirt from '../Tshirt/Tshirt';
 
 const Home = () => {
     const tshirts = useLoaderData();
+
+    // handler for Buy this
+    const handleAddToCart = tshirt => {
+        console.log(tshirt);
+    }
+
     return (
         <div className='home-container'>
             <div className="t-shirt-container">
@@ -11,11 +19,12 @@ const Home = () => {
                     tshirts.map(tshirt => <Tshirt
                         key={tshirt.id}
                         tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
                     ></Tshirt>)
                 }
             </div>
             <div className="cart-container">
-
+                <Cart></Cart>
             </div>
         </div>
     );
--- src/componants/Tshirt/Tshirt.js
+++ src/componants/Tshirt/Tshirt.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Tshirt.css'
 
-const Tshirt = ({ tshirt }) => {
-    const { name, id, picture } = tshirt;
+const Tshirt = ({ tshirt, handleAddToCart }) => {
+    const { name, picture, price } = tshirt;
     return (
-        <div>
+        <div className='t-shirt'>
             <img src={picture} alt="" />
+            <h2>{name}</h2>
+            <h4>Price: {price}</h4>
+            <button onClick={() => handleAddToCart(tshirt)}>Buy this</button>
         </div>
     );
 };
